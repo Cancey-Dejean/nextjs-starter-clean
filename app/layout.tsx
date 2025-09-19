b1933e04c93ac3563bf6cd910c543086d2984591
Cancey-Dejean <canceyd@gmail.com>
2025-09-19 09:01:51 -0500

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { inter } from "@/utils/fonts";
+import Providers from "@/components/DarkLightMode/providers";
+import "@/styles/globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,11 +14,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.variable}>
+        <Providers>
+          <div className="grid min-h-[100dvh] grid-rows-[auto_1fr_auto]">
+            <header>Header</header>
+            <main>{children}</main>
+            <footer>Footer</footer>
+          </div>
+        </Providers>
       </body>
     </html>
   );
